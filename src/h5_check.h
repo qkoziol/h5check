36fa1a61708030339a7b9b34377b8dbc9c61046a
Albert Cheng <acheng@hdfgroup.org>
2011-08-17 09:05:17 -0500

    Added/fixed h5check copyright notices.
    Tested: jam.

--- src/h5_check.h
+++ src/h5_check.h
@@ -1,3 +1,14 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of h5check. The full h5check copyright notice,          *
+ * including terms governing use, modification, and redistribution, is       *
+ * contained in the file COPYING, which can be found at the root of the      *
+ * source code distribution tree.  If you do not have access to this file,   *
+ * you may request a copy from help@hdfgroup.org.                            *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include "h5chk_config.h"
 #include "h5check_public.h"
 #include <stdio.h>
--- src/h5check_public.h
+++ src/h5check_public.h
@@ -1,3 +1,14 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of h5check. The full h5check copyright notice,          *
+ * including terms governing use, modification, and redistribution, is       *
+ * contained in the file COPYING, which can be found at the root of the      *
+ * source code distribution tree.  If you do not have access to this file,   *
+ * you may request a copy from help@hdfgroup.org.                            *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 typedef unsigned long long ck_addr_t;
 typedef unsigned int ck_bool_t;
 typedef int ck_err_t;
